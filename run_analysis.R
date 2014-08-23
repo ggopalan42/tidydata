1143ba70e97f3959498608612b59f94818b3d2ee
Giri Gopalan <ggopalan@yahoo.com>
2014-08-23 16:04:19 -0700

    Fianl version of run_analysis.R and initial version of Codebook

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,16 @@
-# TBD: Opening Comment
+# This R script reads in the training and test data sets,
+# merges them together, extracts mean and std values for
+# all subjects and activities.
+# It then tidies up the data, providing reasonable names
+# for each variable and activity. 
+# Finally the script calculates the average of the means and
+# std for each of the acitvity and subject combination.
+# It then writes out this data to a file called tidy2.txt
 run_analysis <- function () {
-
     # Note: The script assumes that the data are available in the 
     # same dir that the script is run from
     # Read in the data, including the features.txt file
+    print("Reading in data")
     x_train   <- read.table("./X_train.txt")
     y_train   <- read.table("./y_train.txt")
     sub_train <- read.table("./subject_train.txt")
@@ -14,6 +21,7 @@ run_analysis <- function () {
     colnames(features)<-c("Index","FeatureName")
     # activity_name <- read.table("activity_labels.txt",stringsAsFactors=FALSE)
 
+    print("Merging all data")
     # Combine the test and train data. 
     # Column wise combine the training data
     ysx_train <- cbind(y_train,sub_train,x_train)
@@ -24,13 +32,16 @@ run_analysis <- function () {
     # Row wise merge test and training data
     all_raw <- rbind(ysx_train, ysx_test)
 
+    print("Naming all columns")
     # Name the columns of the combined data set 
     colnames(all_raw) <- c("activity", "subjectnum", features$FeatureName)
 
+    print("Extracting only mean and std columns")
     # Extract out only the mean and std columns along with the
     # activity and subject columns
     tidy1<-all_raw[,c(1,2,grep("mean|std",colnames(all_raw)))]
 
+    print("Tidying up data")
     # Change all of the Activity numbers to names
     # Currently this is done in a hard coded way. A future improvement
     # would be to automatically name them based on the activity_names.txt file
@@ -42,7 +53,23 @@ run_analysis <- function () {
     tidy1$activity[tidy1$activity == 6] <- "laying"
 
     # Clean up features names per tidy data guidelines. In this case,
-    # change all col names to lower and remove the characters: -,( and )
-    colnames(tidy1)<-tolower(colnames(tidy1))
+    # remove the characters: -,( and ) from colnames. Also change
+    # mean and std to Camel case so it more readable. Finally, the
+    # cryptic f & t are changed to Freq and Time so its clear which
+    # domain the measurement is made
+    colnames(tidy1)<-sub("mean","Mean",colnames(tidy1))
+    colnames(tidy1)<-sub("std","Std",colnames(tidy1))
     colnames(tidy1)<-gsub("-|\\()","",colnames(tidy1))
+    colnames(tidy1)[3:81]<-sub("f","Freq",colnames(tidy1[3:81]))
+    colnames(tidy1)[3:81]<-sub("t","Time",colnames(tidy1[3:81]))
+
+    # Now create a second tidy dataset that computes the mean of
+    # each of the meand and std variables for every combination
+    # of subject and activity
+    print("Computing averages for each combination of activity and subject")
+    tidy2<-aggregate(. ~ activity + subjectnum, data = tidy1, mean)
+
+    # Write out the final tidy data
+    print("Writing final tidy data to tidy2.txt")
+    write.table(tidy2,"tidy2.txt",row.names=FALSE)
 }
